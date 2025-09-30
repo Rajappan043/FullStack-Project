5ddcd6dd6b0d79bb02bc64d72b7b7276b9f2fc03
Rajappan043 <rajappaambal@gmail.com>
2025-09-30 14:04:54 +0530

    first commit

--- server/routes/result.js
+++ server/routes/result.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Result = require('../models/Result');
+const Result = require('../models/result');
 const Exam = require('../models/exam');
 const { auth, isAdmin } = require('../middleware/auth');
 
